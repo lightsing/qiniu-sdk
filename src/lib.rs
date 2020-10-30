a4bde5ac6a4a67d4fb96ea91328174fdcfca7856
lightsing <light.tsing@gmail.com>
2020-10-30 18:58:49 +0800

    stage

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,12 @@
+use qiniu_sys::Qiniu_Servend_Init;
 
+
+struct Client;
+
+impl Client {
+    fn new_server(access_key: String, secret_key: String) -> Self {
+        Qiniu_Servend_Init(-1);
+
+        Client { }
+    }
+}
